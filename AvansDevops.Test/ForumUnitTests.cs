8b61e8a16d38690835cd9cc8c3b8b7ffd27e004d
Matthijs Feringa <mt.feringa@student.avans.nl>
2024-03-24 20:52:06 +0100

    tested forum composite pattern

--- AvansDevops.Test/ForumUnitTests.cs
+++ AvansDevops.Test/ForumUnitTests.cs
@@ -20,7 +20,69 @@ namespace AvansDevops.Test
             mockForumComposite.AddThread(thread, mockForum);
 
             //Assert
+            Assert.Equal(1, mockForum.Threads.Count);
             Assert.Equal(1, mockForumComposite.GetAllThreadsFromForum(mockForum).Count);
+        }
+
+        [Fact]
+        public void GetAllThreadsFromForum()
+        {
+            //Arrange
+            DiscussionThread thread = new DiscussionThread("First thread");
+            DiscussionThread secondThread = new DiscussionThread("Second thread");
+
+            //Act
+            mockForumComposite.AddThread(thread, mockForum);
+            mockForumComposite.AddThread(secondThread, mockForum);
+
+            //Assert
+            Assert.Equal(2, mockForumComposite.GetAllThreadsFromForum(mockForum).Count);
+        }
+
+        [Fact]
+        public void ThreadCanHavePosts()
+        {
+            //Arrange
+            DiscussionThread thread = new DiscussionThread("First thread");
+            Post post = new Post("First post", mockDeveloper);
+
+            //Act
+            mockForumComposite.AddThread(thread, mockForum);
+            mockForumComposite.AddReactionToThread(post, thread);
+
+            //Assert
+            Assert.Equal(1, thread.Posts.Count);
+        }
+
+        [Fact]
+        public void GetAllReactionsFromThread()
+        {
+            //Arrange
+            DiscussionThread thread = new DiscussionThread("First thread");
+            Post post = new Post("First post", mockDeveloper);
+            Post secondPost = new Post("Second post", mockDeveloper);
+
+            //Act
+            mockForumComposite.AddThread(thread, mockForum);
+            mockForumComposite.AddReactionToThread(post, thread);
+            mockForumComposite.AddReactionToThread(secondPost, thread);
+
+            //Assert
+            Assert.Equal(2, mockForumComposite.GetAllReactionsFromThread(thread).Count);
+        }
+
+        [Fact]
+        public void ForumCanRemoveThreads()
+        {
+            //Arrange
+            DiscussionThread thread = new DiscussionThread("First thread");
+
+            //Act
+            mockForumComposite.AddThread(thread, mockForum);
+            mockForumComposite.RemoveThread(thread, mockForum);
+
+            //Assert
+            Assert.Empty(mockForumComposite.GetAllThreadsFromForum(mockForum));
         }
     }
 }
