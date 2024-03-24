ea4af97c4d9dcbac4a1950103afe8321f6d43192
Matthijs Feringa <mt.feringa@student.avans.nl>
2024-03-24 10:49:20 +0100

    made one test for forum

---
+++ AvansDevops.Test/ForumUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Avans_DevOps.Domain.CompositePattern.ForumCompositePattern;
+using Avans_DevOps.Domain.Forum;
+using Avans_DevOps.Domain.Users;
+
+namespace AvansDevops.Test
+{
+    public class ForumUnitTests
+    {
+        private Forum mockForum = new Forum("Avans DevOps Forum");
+        private ForumComposite mockForumComposite = new ForumComposite();
+        private Developer mockDeveloper = new Developer("timothy", "timothy@mail.com", "mail");
+
+        [Fact]
+        public void ForumCanHaveThreads()
+        {
+            //Arrange
+            DiscussionThread thread = new DiscussionThread("First thread");
+
+            //Act
+            mockForumComposite.AddThread(thread, mockForum);
+
+            //Assert
+            Assert.Equal(1, mockForumComposite.GetAllThreadsFromForum(mockForum).Count);
+        }
+    }
+}
