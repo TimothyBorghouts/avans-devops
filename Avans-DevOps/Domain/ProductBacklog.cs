e1f48195f415ab246682c08fcf0f501debe7e769
Timothy Borghouts <tcr.borghouts@student.avans.nl>
2024-06-23 23:46:31 +0200

    Created tests for ProductBacklog

--- Avans-DevOps/Domain/ProductBacklog.cs
+++ Avans-DevOps/Domain/ProductBacklog.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Avans_DevOps.Domain
+﻿namespace Avans_DevOps.Domain
 {
     public class ProductBacklog
     {
         private List<BacklogItem> backlogItems { get; }
         public ProductBacklog()
         {
-
             backlogItems = new List<BacklogItem>();
         }
 
         public void addItem(BacklogItem backlogItem)
         {
             backlogItems.Add(backlogItem);
+        }
+
+        public IReadOnlyList<BacklogItem> GetBacklogItems()
+        {
+            return backlogItems.AsReadOnly();
         }
     }
 }
---
+++ AvansDevops.Test/ProductBacklogUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using Avans_DevOps.Domain;
+using Avans_DevOps.Domain.Users;
+
+namespace Avans_DevOps.Tests
+{
+    public class ProductBacklogTests
+    {
+        [Fact]
+        public void AddItem_ShouldAddBacklogItemToList()
+        {
+            // Arrange
+            var productBacklog = new ProductBacklog();
+            BacklogItem backlogItem = new BacklogItem("inlog pagina maken",
+            new Developer("timothy", "timothy@gmail.com", "email"));
+
+        // Act
+        productBacklog.addItem(backlogItem);
+
+            // Assert
+            Assert.Contains(backlogItem, productBacklog.GetBacklogItems());
+        }
+
+        [Fact]
+        public void Constructor_ShouldInitializeBacklogItems()
+        {
+            // Arrange & Act
+            var productBacklog = new ProductBacklog();
+
+            // Assert
+            Assert.NotNull(productBacklog.GetBacklogItems());
+            Assert.Empty(productBacklog.GetBacklogItems());
+        }
+    }
+}
